e9dd16f4df384a00dca016c674717c305f951ace
pouria yousefi daneshjoo <164668228+Pouriiayo@users.noreply.github.com>
2025-12-02 23:22:58 +0330

    Add files via upload

---
+++ Tamrin 6/Tamrin 6/Program.cs
@@ -0,0 +1,23 @@
+﻿//پوریا یوسفی
+
+int[] arr = { 1, 0, 0, 4, 2, 2, 45, 2, 1, };
+Movezero(arr);
+foreach (int i in arr)
+{
+    Console.Write(i);
+}
+void Movezero(int[] arr)
+{
+    int index = 0;
+    int temp;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != 0)
+        {
+            temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+            index++;
+        }
+    }
+}
